ca52b5461b3ef54a24b42abc98c94d5565164a09
David Surkov <surkovdavid@gmail.com>
2025-11-23 13:10:23 +0100

    Day 2

--- Tasks/Day2/4-memoize.js
+++ Tasks/Day2/4-memoize.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 // implement memoize
 
-const memoize = (f) => f;
+const memoize = (f) => {
+  const map = new Map();
+  return (...args) => {
+    const result = f(...args);
+    const cached = map.get(result);
+    if (cached) {
+      return cached;
+    }
+    map.set(result, result);
+    return result;
+  };
+};
 
-const fib = memoize((n) => n <= 1 ? n : fib(n - 1) + fib(n - 2));
+const fib = memoize((n) => (n <= 1 ? n : fib(n - 1) + fib(n - 2)));
 
 console.log(fib(10));
